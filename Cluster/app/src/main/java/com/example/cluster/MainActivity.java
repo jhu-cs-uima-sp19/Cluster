2f8c71e8a9c8696171edc43dc3e1f4951c7ae459
Nick Lemanski <qb12colonial@gmail.com>
2019-04-09 15:03:30 -0400

    Created Fragments, ready for custom list views to be added

--- Cluster/app/src/main/java/com/example/cluster/MainActivity.java
+++ Cluster/app/src/main/java/com/example/cluster/MainActivity.java
@@ -1,16 +1,14 @@
 package com.example.cluster;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -19,18 +17,13 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_Manage);
-                    Intent intent = new Intent(MainActivity.this, Manage.class);
-                    startActivity(intent);
-
+                    loadFragment(new ManageFragment());
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_Interested);
+                    loadFragment(new InterestedFragment());
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_Find);
-                    Intent intent2 = new Intent(MainActivity.this, Find.class);
-                    startActivity(intent2);
+                    loadFragment(new FindFragment());
                     return true;
             }
             return false;
@@ -42,9 +35,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        //loading the default fragment
+        loadFragment(new ManageFragment());
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment).commit();
+            return true;
+        }
+        return false;
     }
 
 }
