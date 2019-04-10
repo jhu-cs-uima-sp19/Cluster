a0496729ef046c9fb6113c67ac5dff5b78982bb8
Matt-Chodaczek-Hopkins <35980749+Matt-Chodaczek-Hopkins@users.noreply.github.com>
2019-04-09 21:47:23 -0400

    Added basic firebase user authentication with email
    Log in, create account, forgot password. Beginnings of a profile activity (change pwd, sign out, delete account, etc)

--- Cluster/app/src/main/java/com/example/cluster/MainActivity.java
+++ Cluster/app/src/main/java/com/example/cluster/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.cluster;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    ImageButton btnSettings;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -38,8 +47,24 @@ public class MainActivity extends AppCompatActivity {
         //loading the default fragment
         loadFragment(new ManageFragment());
 
+        auth = FirebaseAuth.getInstance();
+        btnSettings = (ImageButton) findViewById(R.id.temp_settings_button);
+
+        // if we're already logged in go to the main activity
+        if (auth.getCurrentUser() == null) {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            finish();
+        }
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+            }
+        });
     }
 
     private boolean loadFragment(Fragment fragment) {
---
+++ Cluster/app/src/main/java/com/example/cluster/ProfileActivity.java
@@ -0,0 +1,143 @@
+package com.example.cluster;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class ProfileActivity extends AppCompatActivity {
+    // right now this is very incomplete BUT I am so dang tired
+
+    private Button btnChangePwd, btnDeleteAccount,
+            changePwd, delete, signOut;
+
+    private EditText password, newPassword;
+    private FirebaseAuth.AuthStateListener authListener;
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        //get firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        //get current user
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null) {
+                    // user auth state is changed - user is null
+                    // launch login activity
+                    startActivity(new Intent(ProfileActivity.this, LoginActivity.class));
+                    finish();
+                }
+            }
+        };
+
+        //btnChangePwd = (Button) findViewById(R.id.change_password_button);
+        btnDeleteAccount = (Button) findViewById(R.id.btn_delete_account);
+        //changePwd = (Button) findViewById(R.id.changePwd);
+        signOut = (Button) findViewById(R.id.btn_sign_out);
+
+        //password = (EditText) findViewById(R.id.password);
+        //newPassword = (EditText) findViewById(R.id.newPassword);
+
+        /*btnChangePwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                password.setVisibility(View.GONE);
+                newPassword.setVisibility(View.VISIBLE);
+                changePwd.setVisibility(View.VISIBLE);
+                delete.setVisibility(View.GONE);
+            }
+        });
+
+        changePwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (user != null && !newPassword.getText().toString().trim().equals("")) {
+                    if (newPassword.getText().toString().trim().length() < 6) {
+                        newPassword.setError("Password too short, enter minimum 6 characters");
+                    } else {
+                        user.updatePassword(newPassword.getText().toString().trim())
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()) {
+                                            Toast.makeText(ProfileActivity.this, "Password is updated, sign in with new password!", Toast.LENGTH_SHORT).show();
+                                            auth.signOut();
+                                        } else {
+                                            Toast.makeText(ProfileActivity.this, "Failed to update password!", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
+                    }
+                } else if (newPassword.getText().toString().trim().equals("")) {
+                    newPassword.setError("Enter password");
+                }
+            }
+        });*/
+
+
+        btnDeleteAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (user != null) {
+                    user.delete()
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(ProfileActivity.this, "We should have a confirmation dialog", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(ProfileActivity.this, LoginActivity.class));
+                                        finish();
+                                    } else {
+                                        Toast.makeText(ProfileActivity.this, "Failed to delete your account!", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth.signOut();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume(); }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (authListener != null) {
+            auth.removeAuthStateListener(authListener);
+        }
+    }
+}
