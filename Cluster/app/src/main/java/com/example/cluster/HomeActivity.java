24ca39dc082fcab24fcc141a8bf290e4ebe6cada
Mason Cole <mason.s.cole@hotmail.com>
2019-04-11 15:58:48 -0400

    disable back button on Home Activity

--- Cluster/app/src/main/java/com/example/cluster/HomeActivity.java
+++ Cluster/app/src/main/java/com/example/cluster/HomeActivity.java
@@ -47,5 +47,10 @@ public class HomeActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+    
+    @Override
+    public void onBackPressed() { //so cannot get back in after signing out
+        //do nothing
+    }
 
 }
