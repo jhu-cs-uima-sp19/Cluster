e0e2ad1afe2c74b1c0b5e7f921e49e1d5f968255
Nick Lemanski <qb12colonial@gmail.com>
2019-04-25 16:36:04 -0400

    Multidex enabled, incorrectly commited last time

--- Cluster/app/src/main/java/com/example/cluster/ApplicationClass.java
+++ Cluster/app/src/main/java/com/example/cluster/ApplicationClass.java
@@ -1,4 +1,10 @@
 package com.example.cluster;
 
-public class ApplicationClass {
+import android.support.multidex.MultiDexApplication;
+
+public class ApplicationClass extends MultiDexApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
 }
