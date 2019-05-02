2712cc6a03115f74d0ffca1ea998298296e53daf
Matt-Chodaczek-Hopkins <35980749+Matt-Chodaczek-Hopkins@users.noreply.github.com>
2019-05-01 22:32:16 -0400

    Username changes via profile activity

--- Cluster/app/src/main/java/com/appcentricity/cluster/ProfileActivity.java
+++ Cluster/app/src/main/java/com/appcentricity/cluster/ProfileActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -18,16 +19,26 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    private Button btnChangePwd, changePwdConf, btnDeleteAccount, btnSignOut;
+    private Button btnChangePwd, changePwdConf, btnDeleteAccount, btnSignOut, btnChangeUName, changeUNameConf;
 
-    private EditText pwd, newPwd;
+    private EditText pwd, newPwd, uName;
+    private TextView uNameDisp;
     private FirebaseAuth.AuthStateListener authListener;
     private FirebaseAuth auth;
+    private FirebaseFirestore db;
 
     private boolean pwdVisible = false;
+    private boolean uNameVisible = false;
 
     private final int MIN_PWD_LEN = 6;
 
@@ -38,6 +49,7 @@ public class ProfileActivity extends AppCompatActivity {
 
         //get firebase auth instance
         auth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
 
         //get current user
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
@@ -54,17 +66,38 @@ public class ProfileActivity extends AppCompatActivity {
             }
         };
 
+        uNameDisp = (TextView) findViewById(R.id.u_name_disp);
+        db.document("users/" + auth.getUid()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                DocumentSnapshot doc = task.getResult();
+                String disp_uname = getResources().getString(R.string.username_placeholder);
+                if(doc.contains("userName")) {
+                    disp_uname += doc.get("userName");
+                } else {
+                    disp_uname += auth.getUid();
+                }
+                uNameDisp.setText(disp_uname);
+            }
+        });
+
         btnChangePwd = (Button) findViewById(R.id.btn_change_pwd);
+        btnChangeUName = (Button) findViewById(R.id.btn_change_uName);
         changePwdConf = (Button) findViewById(R.id.btn_change_pwd_confirm);
+        changeUNameConf = (Button) findViewById(R.id.btn_change_uname_confirm);
         btnDeleteAccount = (Button) findViewById(R.id.btn_delete_account);
         btnSignOut = (Button) findViewById(R.id.btn_sign_out);
 
         pwd = (EditText) findViewById(R.id.pwd);
         newPwd = (EditText) findViewById(R.id.newPwd);
+        uName = (EditText) findViewById(R.id.newUName);
 
         pwd.setVisibility(View.GONE);
         newPwd.setVisibility(View.GONE);
         changePwdConf.setVisibility(View.GONE);
+
+        uName.setVisibility(View.GONE);
+        changeUNameConf.setVisibility(View.GONE);
 
         btnChangePwd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -104,7 +137,7 @@ public class ProfileActivity extends AppCompatActivity {
                                     } else if(pwd.getText().toString().trim().equals(newPwd.getText().toString().trim())) {
                                         newPwd.setError(getString(R.string.same_pwd));
                                     } else {
-                                        new AlertDialog.Builder(ProfileActivity.this, android.app.AlertDialog.THEME_TRADITIONAL)
+                                        new AlertDialog.Builder(ProfileActivity.this, R.style.AlertDialogTheme)
                                                 .setTitle(R.string.change_pwd_dialogue)
                                                 .setMessage(R.string.change_pwd_logout)
                                                 .setPositiveButton(R.string.sure, new DialogInterface.OnClickListener()
@@ -140,11 +173,57 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
+        btnChangeUName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!uNameVisible) {
+                    uName.setVisibility(View.VISIBLE);
+                    changeUNameConf.setVisibility(View.VISIBLE);
+                    uNameVisible = true;
+                } else {
+                    uName.setVisibility(View.GONE);
+                    changeUNameConf.setVisibility(View.GONE);
+                    uNameVisible = false;
+                }
+            }
+        });
+
+        changeUNameConf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String input = uName.getText().toString().trim();
+                // we can come up with more complicated conditions for usernames if we want, I don't care
+                if (input.length() == 0) {
+                    uName.setError(getString(R.string.bad_uname));
+                } else {
+                    new AlertDialog.Builder(ProfileActivity.this, R.style.AlertDialogTheme)
+                            .setTitle(R.string.change_uname)
+                            .setMessage(R.string.change_uname_msg)
+                            .setPositiveButton(R.string.sure, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    String newUserName = uName.getText().toString();
+                                    Map<String, Object> updateUserName = new HashMap<>();
+                                    updateUserName.put("userName", newUserName);
+                                    db.document("users/" + auth.getUid()).set(updateUserName, SetOptions.merge());
+                                    String updatedDispUserName = getResources().getString(R.string.username_placeholder) + input;
+                                    uNameDisp.setText(updatedDispUserName);
+                                    uName.setText("");
+                                    uName.setVisibility(View.GONE);
+                                    changeUNameConf.setVisibility(View.GONE);
+                                    uNameVisible = false;
+                                }
+                            })
+                            .setNegativeButton(R.string.not_sure, null)
+                            .show();
+                }
+            }
+        });
 
         btnDeleteAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new AlertDialog.Builder(ProfileActivity.this, android.app.AlertDialog.THEME_TRADITIONAL)
+                new AlertDialog.Builder(ProfileActivity.this, R.style.AlertDialogTheme)
                         .setTitle(R.string.delete_account)
                         .setMessage(R.string.delete_account_confirm)
                         .setPositiveButton(R.string.sure, new DialogInterface.OnClickListener()
@@ -177,7 +256,7 @@ public class ProfileActivity extends AppCompatActivity {
         btnSignOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new AlertDialog.Builder(ProfileActivity.this, android.app.AlertDialog.THEME_TRADITIONAL)
+                new AlertDialog.Builder(ProfileActivity.this, R.style.AlertDialogTheme)
                         .setTitle(R.string.sign_out_dialog)
                         .setMessage(R.string.sign_out_confirm)
                         .setPositiveButton(R.string.sure, new DialogInterface.OnClickListener()
